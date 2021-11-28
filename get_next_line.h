dc7e9532031e1c3cac110ef0ab1f274407413fb9
Muxa172002 <muxa172002@yandex.ru>
2021-11-28 19:25:19 +0300

    first

---
+++ get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyelena <cyelena@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/14 17:28:42 by cyelena           #+#    #+#             */
+/*   Updated: 2021/11/28 19:07:44 by cyelena          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE
+# endif
+
+char	*get_next_line(int fd);
+char	*get_line(int fd, char *remains, char *buf);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*after_n(char *remains);
+char	*befor_n(char *remains);
+int		found_n(char *remains);
+#endif
